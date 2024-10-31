ba0ebb83f7325392c6f5387bf5b80dcd9879d3b5
dxy1234567 <dxy0020702@gmail.com>
2024-10-31 14:20:55 +0000

    update

--- utils/util.py
+++ utils/util.py
@@ -17,3 +17,15 @@ def plot_losses(train_losses, val_losses, path_output):
 
     # 保存图像
     plt.savefig(path_output)
+
+def print_progress(i, N):
+    """
+    打印当前进度。
+
+    Param:
+        i: 当前进度（第i次迭代）
+        N: 总迭代次数
+    """
+    if i % 100 == 0 or i == N:  # 每 100 次迭代或最后一次迭代时更新进度
+        run = i / N * 100
+        print("Runing: {:.2f}% done.".format(run))
